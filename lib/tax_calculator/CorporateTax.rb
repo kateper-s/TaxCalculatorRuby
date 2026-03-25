47451b2835ad1868b7148bbe7ee21600646e9643
Mateoooo <alenakulikova506@gmail.com>
2026-03-25 21:06:15 +0300

    add the implementation of depreciation and advance payments

--- lib/tax_calculator/CorporateTax.rb
+++ lib/tax_calculator/CorporateTax.rb
@@ -57,4 +57,123 @@ module TaxCalculator
         quarterly_breakdown: calculate_quarterly
       }
     end
+
+    def calculate_depreciation(asset_value:, useful_life:, method: :linear)
+      case method
+      when :linear
+        annual_depreciation = asset_value / useful_life
+        monthly_depreciation = annual_depreciation / 12
+        
+        {
+          annual: annual_depreciation,
+          monthly: monthly_depreciation,
+          schedule: (1..useful_life).map do |year|
+            { 
+              year: year, 
+              depreciation: annual_depreciation,
+              remaining_value: asset_value - (annual_depreciation * year)
+            }
+          end
+        }
+      when :declining_balance
+        rate = 2.0 / useful_life
+        calculate_declining_balance(asset_value, useful_life, rate)
+      end
+    end
+
+    def calculate_advance_payments
+      quarterly_tax = calculate_profit_tax(period: :quarter)
+      
+      {
+        q1: quarterly_tax[:tax_amount] * 0.25,
+        q2: quarterly_tax[:tax_amount] * 0.25,
+        q3: quarterly_tax[:tax_amount] * 0.25,
+        q4: quarterly_tax[:tax_amount] * 0.25,
+        total: quarterly_tax[:tax_amount],
+        monthly_payments: calculate_monthly_payments(quarterly_tax[:tax_amount])
+      }
+    end
+
+    def tax_optimization_suggestions
+      current_tax = calculate_profit_tax
+      
+      suggestions = []
+      
+      if @transactions.sum { |t| t[:amount] if t[:type] == :income } < 150_000_000
+        suggestions << {
+          type: :system_change,
+          suggestion: "Consider switching to USN",
+          potential_savings: compare_tax_systems(:usn_income)
+        }
+      end
+      
+      if has_investment_expenses?
+        suggestions << {
+          type: :investment_benefit,
+          suggestion: "Apply investment tax credit",
+          potential_savings: calculate_investment_credit
+        }
+      end
+      
+      if @losses_previous_years.any?
+        suggestions << {
+          type: :losses_carryforward,
+          suggestion: "Apply losses from previous years",
+          potential_savings: calculate_losses_benefit
+        }
+      end
+      
+      suggestions
+    end
+
+    def compare_tax_systems
+      results = {}
+      
+      TAX_SYSTEMS.each do |system, info|
+        results[system] = calculate_for_system(system)
+      end
+      
+      results.sort_by { |_, data| data[:total_tax] }.to_h
+    end
+
+    def generate_report(format: :json)
+      report_data = {
+        company_info: {
+          tax_system: @tax_system,
+          fiscal_year: @fiscal_year
+        },
+        financials: calculate_profit_tax,
+        advances: calculate_advance_payments,
+        recommendations: tax_optimization_suggestions
+      }
+      
+      case format
+      when :json
+        JSON.pretty_generate(report_data)
+      when :csv
+        generate_csv_report(report_data)
+      when :pdf
+        generate_pdf_report(report_data)
+      end
+    end
+
+    private
+
+    def calculate_for_system(system)
+      case system
+      when :osn
+        calculate_profit_tax
+      when :usn_income
+        income = sum_transactions_by_type(:income)
+        { total_tax: income * 0.06 }
+      when :usn_income_minus_expenses
+        income = sum_transactions_by_type(:income)
+        expenses = sum_transactions_by_type(:expense)
+        profit = income - expenses
+        min_tax = income * 0.01
+        tax = profit * 0.15
+        { total_tax: [tax, min_tax].max }
+      end
+    end
+  end
 end
